66f06a3c96d0e6e25900c7b41fdf5183c8368b1f
PenyiDavid <penyaskodavid@gmail.com>
2024-10-10 09:54:58 +0200

    Routes

--- database/migrations/2024_10_10_072113_create_answers_table.php
+++ database/migrations/2024_10_10_072113_create_answers_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->unsignedBigInteger('question_id'); // Kapcsolat a kérdéssel
             $table->string('answer'); // Válasz szövege
             $table->boolean('is_correct')->default(false); // Helyes válasz-e
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,40 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScoreController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tesztek', [TestController::class, 'showTesztForm']);   // Teszt form megjelenítése
+Route::post('/tesztek/{tantargy}', [TestController::class, 'startTeszt']); // Teszt kezdés
+Route::post('/tesztkitoltes', [TestController::class, 'submitTeszt']); // Teszt beküldése
+
+// Eredmények megtekintése
+Route::post('/eredmenyem', [ScoreController::class, 'eredmenyem']); // Eredmény oldal
+
+// Új tárgy hozzáadása
+Route::get('/ujtargy', [SubjectController::class, 'create']);
+Route::post('/ujtargy', [SubjectController::class, 'store']);
+
+// Új kérdés hozzáadása
+Route::get('/ujkerdes', [QuestionController::class, 'create']);
+Route::post('/ujkerdes', [QuestionController::class, 'store']);
+
+// Eredmények listázása és szűrése
+Route::get('/eredmenyek', [ScoreController::class, 'listEredmenyek']);
+
+// Kérdések megjelenítése, szerkesztése és törlése
+Route::get('/kerdesek', [QuestionController::class, 'index']);
+Route::get('/kerdesek/{id}', [QuestionController::class, 'edit']);
+Route::post('/kerdesek/{id}', [QuestionController::class, 'update']);
+Route::delete('/kerdesek/{id}', [QuestionController::class, 'destroy']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
